bdc9b110e4179f56f13e6ae2a7dbb1bde0742f6e
itaegjin <tag101@hanmail.net>
2023-01-10 18:40:50 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration  // 구성정보
 public class AppConfig {
     //DIP완성 - 추상에만 의존하면 된다. 이제 구체 클레스는 몰라도 된다.
+    @Bean   // 각 메소드에 빈이라 적용하면 스프링 컨테이너에 등록이 된다.
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     private static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
-//        return new RateDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
