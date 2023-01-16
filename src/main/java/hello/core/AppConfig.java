045adfb23f63fd36976edc765df10c852a709342
itaegjin <tag101@hanmail.net>
2023-01-16 19:45:06 +0900

    스프링 싱글톤 - 038. @Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,19 +14,26 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration  // 구성정보
 public class AppConfig {
+
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
     //DIP완성 - 추상에만 의존하면 된다. 이제 구체 클레스는 몰라도 된다.
     @Bean   // 각 메소드에 빈이라 적용하면 스프링 컨테이너에 등록이 된다.
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    private static MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
