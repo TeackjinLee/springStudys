1d54793ab6d3b36cdbbbd885415633213cd66956
itaegjin <tag101@hanmail.net>
2023-01-18 12:27:21 +0900

    041. 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // @ComponentScan이 이미 있어서 자동 등록이 된다.
 public class CoreApplication {
 
     public static void main(String[] args) {
