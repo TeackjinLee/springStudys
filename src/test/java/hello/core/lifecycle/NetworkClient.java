e9407607c72c490b13d297cf8ea84a76d3cf2498
itaegjin <tag101@hanmail.net>
2023-01-24 15:22:17 +0900

    055. 빈 등록 초기화, 소멸 메서드

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -4,7 +4,7 @@ import lombok.ToString;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
 
     private String url;
 
@@ -31,16 +31,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close : " + url);
     }
 
-    @Override   // * implements InitializingBean에서 상속 의존관계 주입이 끝나면 호출해 주겠다.
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    // * implements InitializingBean에서 상속 의존관계 주입이 끝나면 호출해 주겠다.
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destory");
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
