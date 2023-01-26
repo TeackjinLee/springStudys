2470aa3ed3438c7b5336a9d91ab74a7f8805e01d
itaegjin <tag101@hanmail.net>
2023-01-26 22:06:32 +0900

    060. 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -7,49 +7,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
+import javax.inject.Provider;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SingletonWithPrototypeTest1 {
 
+//    @Test
+//    void prototypeFind(){
+//        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+//        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+//        prototypeBean1.addCount();
+//        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+//
+//        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+//        prototypeBean2.addCount();
+//        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+//
+//    }
+
     @Test
-    void prototypeFind(){
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        prototypeBean1.addCount();
-        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+    void providerTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
-        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
-        prototypeBean2.addCount();
-        assertThat(prototypeBean2.getCount()).isEqualTo(1);
-
-    }
-
-    @Test
-    void SingletonClientUserPrototype() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClietnBean.class, PrototypeBean.class);
-
-        ClietnBean clientBean1 = ac.getBean(ClietnBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
         assertThat(count1).isEqualTo(1);
 
-        ClietnBean clientBean2 = ac.getBean(ClietnBean.class);
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
-    static class ClietnBean {
-        private final PrototypeBean prototypeBean;  //생성시점에 주입
+    static class ClientBean {
 
         @Autowired
-        public ClietnBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> provider;  //생성시점에 주입
 
         public int logic() {
+            PrototypeBean prototypeBean = provider.get();
             prototypeBean.addCount();
-            return prototypeBean.getCount();
+            int count = prototypeBean.getCount();
+            return count;
         }
     }
 
