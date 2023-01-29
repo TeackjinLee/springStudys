e4ac7496fd7ebbf40f792ac797eea79d5cadac75
itaegjin <tag101@hanmail.net>
2023-01-29 18:44:16 +0900

    063. 스코프와 Provider

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,32 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    // 핵심은 동시에 여러 요청이 온다 하더라도 요청마다 각각 객체를 따로 관리
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestUrl(requestURL);
+
+        myLogger.log("controller test");
+        Thread.sleep(1000);
+        logDemoService.logic("testId");
+        return "OK";
+    }
+
+}
